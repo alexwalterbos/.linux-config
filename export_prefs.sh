d15e1a98d1f9c86c30f0013136f8d4ee403b0d24
Alex Walterbos <atw.231@gmail.com>
2015-03-21 02:19:27 +0100

    Added 'install now' feature.

--- export_prefs.sh
+++ export_prefs.sh
@@ -2,7 +2,7 @@
 
 function addMissing {
 	if [[ -n $MISSING ]]; then
-		MISSING=$MISSING", "
+		MISSING=$MISSING" "
 	fi
 	MISSING=$MISSING$1
 }
@@ -17,7 +17,15 @@ done
 
 if [[ -n $MISSING ]]; then
 	echo "Please install these programs first: $MISSING.";
-	exit 1;
+	read -p "Want to do so now? " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		`sudo yum -q install $MISSING`
+		if [[ $? -ne 0 ]]; then
+			echo "Yum install failed. Exiting."
+			exit 1
+		fi
+	fi
 fi
 
 echo "Copying files to home directory interactively:"
